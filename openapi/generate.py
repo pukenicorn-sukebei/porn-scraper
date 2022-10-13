435fe6f060043c4b2a4d46290e49d8d35626942d
mhaii <bloo20xd@gmail.com>
2022-10-13 12:06:42 +0700

    generate openapi in yml instead of json

--- openapi/generate.py
+++ openapi/generate.py
@@ -1,18 +1,18 @@
 from fastapi.openapi.utils import get_openapi
-from app.main import app
-import json
+from app import app
+import yaml
 
 
 def generate():
-    with open('openapi/openapi.json', 'w') as f:
-        openapi_json = get_openapi(
+    with open('openapi/openapi.yml', 'w') as f:
+        openapi_spec = get_openapi(
             title=app.title,
             version=app.version,
             openapi_version=app.openapi_version,
             description=app.description,
             routes=app.routes
         )
-        json.dump(openapi_json, f)
+        yaml.dump(openapi_spec, f)
 
 
 if __name__ == "__main__":
